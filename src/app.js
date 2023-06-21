e54e3237e3376b75270109db34ea107a38cf6314
cassialuisa <cassia.pedrosa@rarolabs.com.br>
2023-06-21 16:16:15 -0300

    removes all the routes from app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from './config/dbConnect.js';
-import livros from './models/Livro.js';
 import routes from './routes/index.js';
 
 //preve o erro de conexão
@@ -17,13 +16,4 @@ app.use(express.json());
 
 routes(app);
 
-
-app.get('/livros/:id',(req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-})
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id);
-}
 export default app
