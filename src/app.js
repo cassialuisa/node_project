591720cb20d2a9d7b6d07461996d384b83249ff1
cassialuisa <cassia.pedrosa@rarolabs.com.br>
2023-06-21 12:26:49 -0300

    feat: removes old post on app

--- src/app.js
+++ src/app.js
@@ -17,31 +17,12 @@ app.use(express.json());
 
 routes(app);
 
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis"},
-//     {id: 2, "titulo": "O Hobbit"}
-// ]
-// console.log(livros)
-// app.get('/', (req, res) => {
-//     res.status(200).send('Curso de Node');
-// }) 
-
-// app.get('/livros', (req, res) => {
-//     livros.find((err, livros) => {
-//      res.status(200).json(livros)
-//     })
-// })
 
 app.get('/livros/:id',(req, res) => {
     let index = buscaLivro(req.params.id);
     res.status(200).json(livros[index]);
 })
  
-app.post('/livros', (req, res) => {
-    livros.push(req.body)
-    res.status(201).send('Livro cadastrado com sucesso!')
-})
-
 app.put('/livros/:id',(req, res) => {
     let index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
