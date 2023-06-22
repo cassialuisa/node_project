332ec94118958d172a83beb60237411ddfe54af9
cassialuisa <cassia.pedrosa@rarolabs.com.br>
2023-06-22 13:24:41 -0300

    feat: autors to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import autores from "./autoresRoutes.js";
 import livros from './livrosRoutes.js';
 
 // conjunto de todas as rotas que irei usar
@@ -8,7 +9,8 @@ const routes = (app) => {
     })
     app.use(
         express.json(),
-        livros
+        livros,
+        autores
     )
 }
 
