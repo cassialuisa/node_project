e8ef0cd07219f6ee09e03697571b146e4eb6eac0
cassialuisa <cassia.pedrosa@rarolabs.com.br>
2023-06-23 16:37:38 -0300

    feat: explain data association

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,6 +4,9 @@ const livroSchema = new mongoose.Schema(
     {
         _id: {type: String},
         titulo: {type: String, required: true},
+        //Estamos indicando aqui na chave type que a associação entre os dados irá ocorrer pelo id do autor, 
+        // que foi gerado automaticamente pelo MongoDB ao cadastrar um autor na coleção ‘autores’ através de uma requisição POST, 
+        // representado pelo ObjectId. Além disso, na chave ref estamos fazendo referência ao nome da coleção no banco.
         autor: {type: mongoose.Schema.Types.ObjectId, ref:'autores', required: true},
         editora: {type: String, required: true},
         numeroPaginas: {type: Number}
